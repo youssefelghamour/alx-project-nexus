b3451cdfb1390513ad7e7c6efa893357858d9640
youssef <youssefelghamour@gmail.com>
2026-01-22 20:19:13 +0100

    Add basic viewsets

--- movies/views.py
+++ movies/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import User, Movie, Genre, Rating, WatchHistory
+from .serializers import UserSerializer, MovieSerializer, GenreSerializer, RatingSerializer, WatchHistorySerializer
 
-# Create your views here.
+
+class UserViewSet(viewsets.ModelViewSet):
+    """ Viewset for User model"""
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class MovieViewSet(viewsets.ModelViewSet):
+    """ Viewset for Movie model with rating and watch actions
+        and recommendation features
+    """
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
+
+
+class GenreViewSet(viewsets.ModelViewSet):
+    """ Viewset for Genre model """
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+
+
+class RatingViewSet(viewsets.ModelViewSet):
+    """ Viewset for Rating model """
+    queryset = Rating.objects.all()
+    serializer_class = RatingSerializer
+
+
+class WatchHistoryViewSet(viewsets.ModelViewSet):
+    """ Viewset for WatchHistory model """
+    queryset = WatchHistory.objects.all()
+    serializer_class = WatchHistorySerializer
